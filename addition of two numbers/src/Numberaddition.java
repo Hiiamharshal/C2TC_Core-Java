3c9ae86cbcb0b2950c002a31974ee494f9976842
harsh <harsh@LAPTOP-4HUA2K6V>
2022-03-04 10:49:42 +0530

    first commit

--- addition of two numbers/src/Numberaddition.java
+++ addition of two numbers/src/Numberaddition.java
@@ -3,9 +3,9 @@ public class Numberaddition {
 
 	public static void main(String[] args) {
 		int a=5,b=7,c;
-		System.out.println(a+ " " + b);
+		System.out.print(a+ "+" + b);
 		c=a+b;
-		System.out.println(" " +c);
+		System.out.print("= " +c);
 
 	}
 
